310c0fb5a1cee484ce66904d73c36de895d562b2
Jefferson <jeehferreira444@gmail.com>
2019-07-19 16:37:45 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-let nome =(prompt(`Digite seu nome ,`));{
-    confirm("Este e seu nome " + nome + "?");
-    
-    }
-    let idade = Number(prompt(`Digite sua idade`));{
-        confirm(" Voce tem " + idade + " anos,");
-    
-    }
-    if (idade >= 25 && idade <=60) {
-console.log ("sua idade foi aprovada .");
-    }
-    
-    
-    else{
-        console.log("sua Idade foi reprovada . ")
-    }
-    let salario = Number(prompt("Digite seu salario ."));{
-        confirm("Seu salario é " + salario + "?");
+let nome = (prompt("Digite seu nome"));
+console.log("Seu nome é:" + nome)
 
-    }
-    if (salario>=2000 ){
-        console.log("Seu salario foi aprovado .")
+let idade = Number(prompt('digite sua idade'));
+console.log('sua idade é :' + idade)
 
+if (idade >= 60) {
+    console.log("sua entrada e gratuida")
+}
+else if (idade >= 0 && idade <= 12) {
+    console.log("Vocé paga $12.00.")
+}
+else if (idade >= 13 && idade <= 17) {
+    console.log("Vocé paga $15.00.")
+}
+else if (idade >= 18 && idade <= 59) {
+    let estu = window.confirm("Vocé é estudande")
+    if (estu) {
+        alert("Vocé paga meia R$17,50")
     }
-
-    else{
-        console.log("Seu salrio foi aprovado .");
+    else {
+        alert("Vocé não é estudate então paga R$35,00")
     }
-    console.log("Voce " + nome + ", com o salario de " + salario + ",pode  pedir emprestimo no maximo de " +(salario*10))
+}
